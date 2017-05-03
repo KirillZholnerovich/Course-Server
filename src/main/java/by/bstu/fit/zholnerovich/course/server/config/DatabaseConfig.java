3a1bb9b2a46e2318f1bc6e33e0106631b8412e56
Kirill Zholnerovich <daniel_coover@mail.ru>
2017-05-03 16:44:24 +0300

    DataSource and EntityManager

--- src/main/java/by/bstu/fit/zholnerovich/course/server/config/DatabaseConfig.java
+++ src/main/java/by/bstu/fit/zholnerovich/course/server/config/DatabaseConfig.java
@@ -1,13 +1,21 @@
 package by.bstu.fit.zholnerovich.course.server.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories("by.bstu.fit.zholnerovich.course.server.repository")
@@ -17,5 +25,39 @@ import javax.annotation.Resource;
 public class DatabaseConfig {
 
     @Resource
-    private Environment env;
+    private Environment environment;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource());
+        entityManager.setPackagesToScan(environment.getRequiredProperty("db.entity.package"));
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManager.setJpaProperties(getHibernateProperties());
+
+        return entityManager;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(environment.getRequiredProperty("db.url"));
+        dataSource.setDriverClassName(environment.getRequiredProperty("db.driver"));
+        dataSource.setUsername(environment.getRequiredProperty("db.username"));
+        dataSource.setPassword(environment.getRequiredProperty("db.password"));
+
+        return dataSource;
+    }
+
+    public Properties getHibernateProperties() {
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("hibernate.properties");
+            properties.load(inputStream);
+
+            return properties;
+        } catch (IOException e){
+            throw new IllegalArgumentException("Can't find 'hibernate.properties' in classpath!", e);
+        }
+    }
 }
